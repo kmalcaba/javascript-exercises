8a2cba7084f50cb0460a21071ffae64d61662595
Kirsten <kmalcaba@gmail.com>
2022-05-14 16:43:38 -0700

    Find oldest person in array of objects

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,13 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(people) {
+    let oldestIdx = 0;
+    let oldestAge = 0;
+    for (person of people) {
+        const age = person.yearOfDeath - person.yearOfBirth;
+        if(age > oldestAge)
+            oldestIdx = people.indexOf(person);
+        oldestAge = Math.max(oldestAge, age);
+    }
+    return people[oldestIdx];
 };
 
 // Do not edit below this line
