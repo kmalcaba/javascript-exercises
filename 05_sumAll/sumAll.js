58dda7b6526a3443a343936dc6a971cb43ea3983
Kirsten <kmalcaba@gmail.com>
2022-05-14 15:31:09 -0700

    Add function to sum all given ranges

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,11 @@
-const sumAll = function() {
-
+const sumAll = function(num1, num2) {
+    if (isNaN(num1) || isNaN(num2))
+        return "ERROR";
+    let sum = 0;
+    for(let i = num1; i <= num2; i++)
+        sum += i;
+    
+    return sum;
 };
 
 // Do not edit below this line
