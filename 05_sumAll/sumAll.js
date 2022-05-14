928c25d0df8e455623551cbaae4895a6bf163397
Kirsten <kmalcaba@gmail.com>
2022-05-14 15:38:57 -0700

    Add start and end range, and validation

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,9 +1,11 @@
 const sumAll = function(num1, num2) {
-    if (isNaN(num1) || isNaN(num2))
+    if (typeof num1 != "number" || typeof num2 != "number" || num1 < 0 || num2 < 0)
         return "ERROR";
     let sum = 0;
-    for(let i = num1; i <= num2; i++)
-        sum += i;
+    let startRange = num1 < num2 ? num1 : num2;
+    let endRange = num1 > num2 ? num1 : num2;
+    for(startRange; startRange <= endRange; startRange++)
+        sum += startRange;
     
     return sum;
 };
