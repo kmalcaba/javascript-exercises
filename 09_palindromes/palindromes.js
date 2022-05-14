df02362dbbd178c0875e083f427d9473e0a95232
Kirsten <kmalcaba@gmail.com>
2022-05-14 16:27:21 -0700

    Add palindrome functionality

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,12 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+    str = str.toLowerCase();
+    str = str.replace(/[^a-zA-Z0-9]/g, "")
+    let end = str.length - 1;
+    for (let start = 0; start <= end; start++, end--) {
+        if(str.charAt(start) !== str.charAt(end))
+            return false;
+    }
+    return true;
 };
 
 // Do not edit below this line
