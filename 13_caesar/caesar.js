a885f6d8ee6c6f66401ff7c154d3d51b1d2d07c6
Kirsten <kmalcaba@gmail.com>
2022-05-14 19:56:21 -0700

    Exclude punctuations and spaces

--- 13_caesar/caesar.js
+++ 13_caesar/caesar.js
@@ -2,8 +2,16 @@ const caesar = function(str, num) {
     let newStr = '';
 
     for (let i = 0; i < str.length; i++) {
-        newStr += String.fromCharCode(str.charCodeAt(i) + num);
-    }
+        const c = str.charAt(i);
+        if(c.match(/[a-z]/i)) {
+            let charCode = str.charCodeAt(i) + num;
+            if ((charCode > 90 && charCode < 97) || (charCode > 122 && charCode < 97) )
+                charCode -= 26;
+            newStr += String.fromCharCode(charCode);
+        }
+        else
+            newStr += c;
+    }  
 
     return newStr;
 };
