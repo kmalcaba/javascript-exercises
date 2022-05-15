4868738cc7a635d28922d7831fcf8c909b9eb8c4
Kirsten <kmalcaba@gmail.com>
2022-05-14 19:27:26 -0700

    Add caesar cipher function

--- 13_caesar/caesar.js
+++ 13_caesar/caesar.js
@@ -1,5 +1,11 @@
-const caesar = function() {
+const caesar = function(str, num) {
+    let newStr = '';
 
+    for (let i = 0; i < str.length; i++) {
+        newStr = String.fromCharCode(str.charCodeAt(i) + num);
+    }
+
+    return newStr;
 };
 
 // Do not edit below this line
